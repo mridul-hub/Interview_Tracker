207f000015b88cb6b3ecbf0e27f97dbd1bb250f4
Mridul <mridulgupta12345@gmail.com>
2022-09-13 19:53:36 +0530

    updated103

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://mridul:Kanha@12@cluster0.pnpuz.mongodb.net/node-auth2';
+const dbURI = 'mongodb+srv://mridul:Kanha@12@cluster0.pnpuz.mongodb.net/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true }).then((result) => app.listen(3000))
 .catch((err) => console.log(err));
 
